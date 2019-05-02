9c554bc715015eb986e47609544b93aafb50f71a
Gabriel Trettel <gabrielmtrettel@gmail.com>
2019-05-01 21:54:45 -0300

    teste.py aceita um deretorio como argumento de escrita, alem de criar os arquivos contando em binário

--- teste.py
+++ teste.py
@@ -2,7 +2,15 @@
 import sys
 
 size = sys.argv[1]
+dir  = sys.argv[2]
+
+if dir[-1] != '/':
+    dir += '/'
+
+
 for x in range(int(size)):
-    f = open(f"a{x}", 'w')
+    name = f"{bin(x)[2:]:>15}".replace(' ', '0')
+    
+    f = open(dir + name, 'w')
     f.writelines(f"aaa {x}")
     f.close()
